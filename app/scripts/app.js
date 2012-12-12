9f9bf9b70f7d4f45cf1c26be25e0a015b31ecfa2
Kris Borchers <kris.borchers@gmail.com>
2012-12-12 11:26:14 -0600

    Scroll view to selected speaker

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -58,4 +58,17 @@ var aerogearDevnexusApp = angular.module( "aerogearDevnexusApp", [])
                     }
                 }).stores.sessions
             };
+        }).directive( "jumpTo", function() {
+            return {
+                restrict: 'A',
+                link: function ( scope, element, attributes ) {
+                    if ( scope.speaker.id == scope.$eval( attributes.jumpTo ) ) {
+                        setTimeout( function() {
+                            jQuery( "body" ).animate({
+                                scrollTop: jQuery( "#collapse-" + scope.speaker.id ).offset().top - 40
+                            }, 1500);
+                        }, 100);
+                    }
+                }
+            };
         });
