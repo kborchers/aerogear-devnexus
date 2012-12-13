2e42c5c26d5c4c504790fcbbb905315e166e4f7d
Kris Borchers <kris.borchers@gmail.com>
2012-12-12 22:35:03 -0600

    Simplify jumpTo directive

--- app/scripts/directives/jumpTo.js
+++ app/scripts/directives/jumpTo.js
@@ -1,16 +1,13 @@
 "use strict";
 
 aerogearDevnexusApp.directive( "jumpTo", function() {
-    return {
-        restrict: 'A',
-        link: function ( scope, element, attributes ) {
-            if ( scope.speaker.id == scope.$eval( attributes.jumpTo ) ) {
-                setTimeout( function() {
-                    jQuery( "body" ).animate({
-                        scrollTop: jQuery( "#collapse-" + scope.speaker.id ).offset().top - 40
-                    }, 1500);
-                }, 100);
-            }
+    return function ( scope, element, attributes ) {
+        if ( scope.speaker.id == scope.$eval( attributes.jumpTo ) ) {
+            setTimeout( function() {
+                jQuery( "body" ).animate({
+                    scrollTop: jQuery( "#collapse-" + scope.speaker.id ).offset().top - 40
+                }, 1500);
+            }, 100);
         }
     };
 });
