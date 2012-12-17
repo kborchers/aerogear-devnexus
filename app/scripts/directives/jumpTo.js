5ce94ee073a7af7e6d6a682f321ace86cf9ff386
Kris Borchers <kris.borchers@gmail.com>
2012-12-17 10:05:51 -0600

    Send request type to jumpTo for use in presentations as well as speakers

--- app/scripts/directives/jumpTo.js
+++ app/scripts/directives/jumpTo.js
@@ -2,10 +2,11 @@
 
 aerogearDevnexusApp.directive( "jumpTo", function() {
     return function ( scope, element, attributes ) {
-        if ( scope.speaker.id == scope.$eval( attributes.jumpTo ) ) {
+        var args = scope.$eval( "[" + attributes.jumpTo + "]" );
+        if ( scope[ args[ 1 ] ].id == args[ 0 ] ) {
             setTimeout( function() {
                 jQuery( "body" ).animate({
-                    scrollTop: jQuery( "#collapse-" + scope.speaker.id ).offset().top - 40
+                    scrollTop: jQuery( "#collapse-" + scope[ args[ 1 ] ].id ).offset().top - 40
                 }, 1500);
             }, 100);
         }
